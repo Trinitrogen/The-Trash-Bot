8e5170e92e3f9ba8492d94a5df60cc0ae438d6c7
Kyle <kyle@mbp>
2018-08-02 17:18:21 -0500

    added help

--- TheTrashBot.py
+++ TheTrashBot.py
@@ -10,20 +10,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!Trash'):
+    if message.content.startswith('!trash'):
         Trashbin = open('Dumpster.txt')
         input_list = Trashbin.read().splitlines()
         msg = random.choice(input_list).format(message)
         await client.send_message(message.channel, msg)
         Trashbin.close()
 
-    if message.content.startswith('!Add'):
+    if message.content.startswith('!add'):
         Trashbin = open('Dumpster.txt', 'a')
         input_list = message.content.split()
         input_list.pop(0)
         for line in input_list:
             Trashbin.write(line + "\n")
         Trashbin.close()
+
+    if message.content.startswith('!help'):
+        embed = discord.Embed(title="The Trash Bot", description="The shitposting bot we all deserve", color=0x00ff00)
+        embed.add_field(name="!add", value="Puts a post in the dumpster", inline=False)
+        embed.add_field(name="!trash", value="Picks a post from the dumpster", inline=False)
+        embed.add_field(name="!trashhelp", value="lists all current commands", inline=False)
+
+        msg = 'http://i.imgur.com/aZZTF0r.gifv'.format(message)
+
+        await client.send_message(message.channel, msg)
+        await client.send_message(message.channel, embed=embed)
 
 
 @client.event
