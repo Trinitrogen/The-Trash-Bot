307184ff83e9009913780ce2af602b8b516fffff
Kyle <kyle@mbp>
2018-08-02 16:55:45 -0500

    Added !Add and !Trash

--- TheTrashBot.py
+++ TheTrashBot.py
@@ -1,5 +1,6 @@
 import discord
 import config
+import random
 
 client = discord.Client()
 
@@ -10,8 +11,20 @@ async def on_message(message):
         return
 
     if message.content.startswith('!Trash'):
-        msg = '{0.author.mention} http://i.imgur.com/aZZTF0r.gifv'.format(message)
+        Trashbin = open('Dumpster.txt')
+        input_list = Trashbin.read().splitlines()
+        msg = random.choice(input_list).format(message)
         await client.send_message(message.channel, msg)
+        Trashbin.close()
+
+    if message.content.startswith('!Add'):
+        Trashbin = open('Dumpster.txt', 'a')
+        input_list = message.content.split()
+        input_list.pop(0)
+        for line in input_list:
+            Trashbin.write(line + "\n")
+        Trashbin.close()
+
 
 @client.event
 async def on_ready():
